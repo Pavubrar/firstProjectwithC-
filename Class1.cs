7e9221de363735b337fb8a404f67c83413823973
Pavubrar <56096198+Pavubrar@users.noreply.github.com>
2020-06-10 11:59:26 -0600

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace practicewithcsharp
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 6;
+            if (a + b > 10)
+                Console.WriteLine("The answer is greater than 10");
+            else
+                Console.WriteLine("the answer is not greater than 10");
+        }
+    }
+}
